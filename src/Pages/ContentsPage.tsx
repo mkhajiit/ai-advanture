89e7d015d06a5768f247064491382c200d6f1bfb
mkhajiit <mkhajiit15@gmail.com>
2025-03-12 12:54:00 +0900

    css 제외한 레이아웃 완성

--- src/Pages/ContentsPage.tsx
+++ src/Pages/ContentsPage.tsx
@@ -5,6 +5,10 @@ function ContentsPage({ handleOnClick }: PageProps) {
     <div>
       <button onClick={() => handleOnClick('Main')}>초기 화면으로</button>
       <h1>콘텐츠 페이지</h1>
+      <h2>선택지 지문</h2>
+      <button name='firstButton'>첫번째</button>
+      <button name='SecondButton'>두번째</button>
+      <button name='ThirdButton'>세번째</button>
     </div>
   );
 }

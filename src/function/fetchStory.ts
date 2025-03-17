94ec66743f8546199835df01c586f8899d50bcde
mkhajiit <mkhajiit15@gmail.com>
2025-03-17 20:18:44 +0900

    url 오류 해결

--- src/function/fetchStory.ts
+++ src/function/fetchStory.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+const port = import.meta.env.VITE_ENDPOINT_URL || 5000;
+const baseUrl = port === 5000 ? `http://localhost:${port}` : `https://${port}`;
+
 export async function fetchStroy(
   choice: string,
   isLast: boolean,
@@ -9,15 +12,12 @@ export async function fetchStroy(
 ) {
   try {
     setIsLoading(true);
-    const { data } = await axios.post(
-      `http://${import.meta.env.VITE_ENDPOINT_URL}/generate-story`,
-      {
-        choice,
-        isLast,
-        selectedChoices,
-        numberOfSelection,
-      }
-    );
+    const { data } = await axios.post(`${baseUrl}/generate-story`, {
+      choice,
+      isLast,
+      selectedChoices,
+      numberOfSelection,
+    });
     console.log(data.story); // 방금 그 객체 나옴
     console.log(data.story.text); // undefined 나옴
     return data.story;

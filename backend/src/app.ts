1937bbadf9ae4af92cd396799e654101be87d0bb
mkhajiit <mkhajiit15@gmail.com>
2025-03-17 20:47:13 +0900

    CORS에러

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,9 +13,10 @@ const baseUrl =
   port === 5000 ? `http://localhost:${port}` : `https://${process.env.RAILWAY_PUBLIC_DOMAIN}`;
 let prompt: string;
 
+// CORS 옵션 설정
 const corsOptions = {
   origin: [
-    'http://localhost:5173', // 프론트 로컬 환경에서 요청을 허용
+    'http://localhost:5173', // 로컬 환경에서 요청을 허용
     'https://ai-advanture-416oq1rrm-mkhajiits-projects.vercel.app', // Vercel 배포 도메인 허용
   ],
   methods: ['GET', 'POST'], // 허용할 HTTP 메서드
@@ -26,14 +27,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 // JSON 형식의 요청 본문을 파싱(parsing) 하기 위한 미들웨어
 app.use(express.json());
-
-// OPTIONS 요청을 처리하는 미들웨어 (CORS 헤더 설정)
-app.options('*', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // 또는 특정 도메인
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.sendStatus(204); // No Content 응답
-});
 
 app.post('/generate-story', async (req, res) => {
   const { choice, isLast, selectedChoices, numberOfSelection } = req.body;
@@ -48,7 +41,6 @@ app.post('/generate-story', async (req, res) => {
   }
   const response = await callOpenAI(prompt);
   // 요청 처리 로직
-  res.setHeader('Access-Control-Allow-Origin', '*'); // 여기서 모든 도메인 허용
   res.send({ message: '성공적으로 처리되었습니다.', story: response });
 });
 

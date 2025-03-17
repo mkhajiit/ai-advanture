3d74099516c24b6fd1e9ee55da8ffd609a9d8417
mkhajiit <mkhajiit15@gmail.com>
2025-03-17 22:52:49 +0900

    에러 해결

--- src/function/fetchStory.ts
+++ src/function/fetchStory.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const port = import.meta.env.VITE_ENDPOINT_URL || 5000;
-// const baseUrl = port === 5000 ? `http://localhost:${port}` : `https://${port}`;
+const port = import.meta.env.VITE_ENDPOINT_URL || 5000;
+const baseUrl = port === 5000 ? `http://localhost:${port}` : `https://${port}`;
 
 export async function fetchStroy(
   choice: string,
@@ -12,15 +12,12 @@ export async function fetchStroy(
 ) {
   try {
     setIsLoading(true);
-    const { data } = await axios.post(
-      'https://ai-advanture-production.up.railway.app/generate-story',
-      {
-        choice,
-        isLast,
-        selectedChoices,
-        numberOfSelection,
-      }
-    );
+    const { data } = await axios.post(`${baseUrl}/generate-story`, {
+      choice,
+      isLast,
+      selectedChoices,
+      numberOfSelection,
+    });
     console.log(data.story); // 방금 그 객체 나옴
     console.log(data.story.text); // undefined 나옴
     return data.story;
